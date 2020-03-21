bb537969b98900924ecdabaa41f9a6ff57b2b109
san-soucie <44901782+san-soucie@users.noreply.github.com>
2020-03-20 22:13:48 -0400

    Additional tests for bad inputs to block_diagonal

--- test/utils.jl
+++ test/utils.jl
@@ -55,5 +55,24 @@ bd_mix = Float64[1.0  2.0  0.0  0.0  0.0  0.0  0.0  0.0;
                  0.0  0.0  0.0  0.0  0.0 11.0 12.0 13.0;
                  0.0  0.0  0.0  0.0  0.0 14.0 15.0 16.0;]
 
+
+empty_mats = [[1 2; 3 4], [5 6], [], [7], [8 9 10; 11 12 13; 14 15 16]]
+bd_emp = [1  2  0  0  0  0  0  0;
+          3  4  0  0  0  0  0  0;
+          0  0  5  6  0  0  0  0;
+          0  0  0  0  7  0  0  0;
+          0  0  0  0  0  8  9 10;
+          0  0  0  0  0 11 12 13;
+          0  0  0  0  0 14 15 16;]
+
+bad_3d_array = [rand(3), rand(3, 4), rand(3, 4, 5)]
+bad_out_size_mat = [[1 2; 3 4], [5 6; 7 8]]
+m = [1 2 0 0; 3 4 0 0; 0 0 5 6; 0 0 7 8]
+mb = [1 2 0 ; 3 4 0 ; 0 0 5 ; 0 0 7 ]
+
 @test Distributions.block_diagonal(mats) == bd_mat
 @test Distributions.block_diagonal(mixed_mats) == bd_mix
+@test Distributions.block_diagonal(empty_mats) == bd_emp
+@test_throws ArgumentError Distributions.block_diagonal(bad_3d_array)
+@test Distributions.block_diagonal!(bad_out_size_mat, m) == m
+@test_throws ArgumentError  Distributions.block_diagonal!(bad_out_size_mat, mb)
